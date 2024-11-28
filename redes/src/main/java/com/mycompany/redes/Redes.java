4f036e1b397c4826636ac924152ea53126732676
Parker050 <149011442+Parker050@users.noreply.github.com>
2024-11-28 01:26:39 -0600

    Update Redes.java
    1.- Implementacion de funcion "RedDeIP" --> Retorna a que red corresponde la direcion IP incluso si pertenece a una seb red
    
    2.- Implementacion de fincion "noNodosYBroadcast" --> retorna el numero de nodos que hay en la red

--- redes/src/main/java/com/mycompany/redes/Redes.java
+++ redes/src/main/java/com/mycompany/redes/Redes.java
@@ -32,6 +32,82 @@ public class Redes {
     }
 }
 
+//Funcion Red de la IP, Retorna a que red pertenece la direccion IP, incluso si la red esta segmentada
+public static String redDeIp(String Ip, String mascara){
+        //Separa los bytes 
+        String[] bitesMasc = mascara.split("\\.");
+        String[] bitesIp = Ip.split("\\.");
+        String bite = "";
+        String red = "";
+        
+        for (int i = 0; i <= bitesMasc.length-1; i++) {
+            //Masc
+            int numeroEntero = Integer.parseInt(bitesMasc[i]);
+            String numeroBinario = Integer.toBinaryString(numeroEntero);
+            System.out.println("Marcara: "+numeroBinario);
+            while (numeroBinario.length() != 8) {
+                //Se agrega un 0 al inicoi de la cadena para completar los o byts y hacer el AND correctamente
+                numeroBinario = 0 +numeroBinario;
+            }
+            System.out.println("Mascara: "+numeroBinario);
+            //IP
+            int numeroEnteroIp = Integer.parseInt(bitesIp[i]);
+            String numeroBinarioIP = Integer.toBinaryString(numeroEnteroIp);
+            System.out.println("IP: "+numeroBinarioIP);
+            while (numeroBinarioIP.length() != 8) {
+                //Se agrega un 0 al inicoi de la cadena para completar los o byts y hacer el AND correctamente
+                numeroBinarioIP = 0 + numeroBinarioIP;
+            }
+            System.out.println("IP: "+numeroBinarioIP);
+            //AND
+            for (int x = 0; x < numeroBinario.length(); x++) {
+                char bitMasc = numeroBinario.charAt(x);
+                char bitIp = numeroBinarioIP.charAt(x);
+                if (bitMasc == '1' && bitIp == '1') {
+                    bite += "1";
+                }else
+                    bite +="0";
+            }
+            
+            BigInteger numeroBigInteger = new BigInteger(bite, 2); // El segundo argumento 2 indica base 2 (binario)
+            String numeroDecimal = numeroBigInteger.toString(10); // El segundo argumento 10 indica base 10 (decimal)
+            red += numeroDecimal+".";
+            bite = "";
+        }
+        return "La red es: "+red.substring(0, red.length()-1);
+    }
+    //Funcion nodos, retorna el numero de nodos que hay en la red
+    public static void noNodosYBroadcast(String Ip, String mascara){
+        int bits = 0;
+        int noIps = 0;
+        String[] bites = mascara.split("\\.");
+        
+        for (int i = 3; i >= 0; i--) {
+            if (bites[i].equals("0")) {
+                bits += 8;
+            }
+            else{
+                switch (bites[i]) {
+                    case "128" -> bits += 7;
+                    case "192" -> bits += 6;
+                    case "224" -> bits += 5;
+                    case "240" -> bits += 4;
+                    case "248" -> bits += 3;
+                    case "252" -> bits += 2;
+                    case "254" -> bits += 1;
+                    case "255" -> {
+                    }
+                    default -> throw new AssertionError();     
+                }
+            }
+        }
+        System.out.println(bits);
+        noIps = (int) Math.pow(2, bits) -2;
+        System.out.println("Numero de nodos disponibles = "+noIps);
+    }
+
+
+//Clase Result Made by DANIELA ---------------------------------------------------------------
 class Result {
     // Declara global para usarla en el main
     public String resultadoClase;
